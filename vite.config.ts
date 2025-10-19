f67cc3bb93ba9d62676d856df295397e5ba19293
Lazaro Yunier <lazaroyunier96@gmail.com>
2025-10-19 08:47:02 -0400

    fix: correct project name casing and simplify Vite base path configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,9 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path"
 
-// https://vite.dev/config/
-export default defineConfig(({command})=>{
+export default defineConfig(()=>{
   return {
-  base: command === 'serve' ? '/' : '/Portafolio/',
+  base: '/' ,
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
